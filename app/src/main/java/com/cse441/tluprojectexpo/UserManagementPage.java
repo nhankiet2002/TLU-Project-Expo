7101b99232f5e01564598d96366fb53bff60c277
TuanNghiaDo <dotuannghia07@gmail.com>
2025-06-03 02:06:22 +0700

    Init Catalog Management Page and Rename User Management Page

---
+++ app/src/main/java/com/cse441/tluprojectexpo/UserManagementPage.java
@@ -0,0 +1,14 @@
+package com.cse441.tluprojectexpo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserManagementPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_management_page);
+    }
+}
