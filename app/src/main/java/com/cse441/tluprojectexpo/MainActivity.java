93dfef510290910f90be276e22f8c3cd51ff54be
nhankiet2002 <nhankiet2002@gmail.com>
2025-06-03 11:13:39 +0700

    rename package

--- app/src/main/java/com/cse441/tluprojectexpo/MainActivity.java
+++ app/src/main/java/com/cse441/tluprojectexpo/MainActivity.java
@@ -1,24 +1,83 @@
 package com.cse441.tluprojectexpo;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TableLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.cse441.tluprojectexpo.fragment.ViewPagerAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        viewPager = findViewById(R.id.view_pager);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageScrolled(int position,
+                                       float positionOffset,
+                                       int positionOffsetPixels)
+            {}
+
+            public void onPageSelected(int position){
+                switch (position){
+                    case 0:
+                        bottomNavigationView.getMenu().findItem(R.id.menu_home).setChecked(true);
+                        break;
+                    case 1:
+                        bottomNavigationView.getMenu().findItem(R.id.menu_create).setChecked(true);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                // SỬA LỖI 2: Sử dụng if-else if để tránh lỗi "constant expression required"
+                if (itemId == R.id.menu_home) {
+                    viewPager.setCurrentItem(0);
+                    return true; // Quan trọng: trả về true để đánh dấu đã xử lý
+                } else if (itemId == R.id.menu_create) {
+                    viewPager.setCurrentItem(1);
+                    return true; // Quan trọng: trả về true
+                }
+                return false; // Trả về false nếu không có item nào khớp (hoặc true tùy logic)
+            }
         });
     }
 }
