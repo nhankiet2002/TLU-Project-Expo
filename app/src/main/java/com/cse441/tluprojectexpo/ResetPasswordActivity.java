1db9ca8a1ff0a163489fc473d7afaaead7fb8dda
Tranthunguyet205 <tranminhnguyet205@gmail.com>
2025-06-14 00:36:10 +0700

    Update file ResetPasswordActivity.java

--- app/src/main/java/com/cse441/tluprojectexpo/ResetPasswordActivity.java
+++ app/src/main/java/com/cse441/tluprojectexpo/ResetPasswordActivity.java
@@ -1,4 +1,14 @@
 package com.cse441.tluprojectexpo;
 
-public class ResetPasswordActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setContentView(R.layout.reset_password);
+    }
 }
