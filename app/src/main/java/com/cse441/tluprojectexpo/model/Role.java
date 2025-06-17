843151f8d9d7d6c064f1183a26a7d1cedecabbb1
TuanNghiaDo <dotuannghia07@gmail.com>
2025-06-17 15:40:03 +0700

    Create Role.java

---
+++ app/src/main/java/com/cse441/tluprojectexpo/model/Role.java
@@ -0,0 +1,19 @@
+package com.cse441.tluprojectexpo.model;
+
+import com.google.firebase.firestore.DocumentId;
+
+public class Role {
+    @DocumentId
+    private String roleId;
+    private String roleName;
+
+    public Role(){}
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+}
